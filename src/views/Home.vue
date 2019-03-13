b0b7bda5b046f119a54244f7b87c17431310209e
Joshua Armstrong <josarmst@wkmau4444805.global.publicisgroupe.net>
2019-03-13 17:20:18 +1100

    [vue]

---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <h1>
+      Joshua & Stephanie<br/>
+      November 2nd, 2019<br/>
+      Narbethong, Vic<br/>
+      #HappilyEverArmstrong</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+<style lang="scss">
+  .home {
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+</style>
