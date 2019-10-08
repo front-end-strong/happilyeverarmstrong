6a6b076d402d12b785dd8f98b50e7f49e1f6ef00
Joshua Armstrong <frontendstrong@gmail.com>
2019-10-08 16:35:16 +1100

    .

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.af9c88fd2f80fdf86a38a0e713dd20a0.js"
+  "/precache-manifest.f535c663225b4edfa7f7325836fa6915.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "happilyeverarmstrong"});
